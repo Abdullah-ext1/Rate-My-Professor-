24eac036fa8d64340049ea7603fccf051f8eff77
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-03-17 23:45:37 +0530

    final clean structure

---
+++ server/src/db/db.connection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./dbname";
+
+const connectDb = async () => {
+    try {
+       const connection =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDb connected !! ${connection.connection.host}`);
+        
+    } catch (error) {
+        console.log("Error : MONGODB CONNECTION ERROR!!" , error)
+        process.exit(1)
+    }
+}
+
+export {connectDb}
---
+++ server/src/db/dbname.js
@@ -0,0 +1,3 @@
+const DB_NAME = "Rateit"
+
+export {DB_NAME}
