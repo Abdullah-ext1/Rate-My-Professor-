1836d52ad2d68b705a9063eeda158e63d6ab8df8
Ammarfrfr <157887137+Ammarfrfr@users.noreply.github.com>
2026-03-20 23:30:49 +0530

    comment controller done

--- server/src/controller/comment.controller.js
+++ server/src/controller/comment.controller.js
@@ -3,7 +3,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Comment } from "../models/comment.models.js";
 import { Post } from "../models/post.models.js";
-import { User } from "../models/users.models.js";
 
 const createAComment = asyncHandler(async(req, res) => {
   const postId = req.params.postId;
@@ -31,7 +30,41 @@ const createAComment = asyncHandler(async(req, res) => {
   )
 }) 
 
+const replyToAComment = asyncHandler(async(req, res) => {
+  const commentId = req.params.commentId
+  const { content } = req.body
+
+  if(content.length == 0){
+    throw new ApiError(401, "Comment is neccesarry to make a comment")
+  }
+
+  const comment = await Comment.findById(commentId)
+  if(!comment){
+    throw new ApiError(404, "Comment not found")
+  } 
+
+  const commentCreate = await Comment.create({
+    post: comment.post,
+    content,
+    user: req.user.id
+  })
+
+  const commentReply = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $push: { replies: commentCreate._id }
+    },
+    { new: true }
+    )
+
+  return res
+  .status(201)
+  .json(
+    new ApiResponse(201, commentReply, "Reply was created Successfully")
+  )
+})
+
 export {
   createAComment,
-
+  replyToAComment,
 }
