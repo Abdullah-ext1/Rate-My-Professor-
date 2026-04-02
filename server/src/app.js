fe7230609e186f5c1e94c637f97c21cf8634f2c7
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-04-02 16:49:36 +0530

    socket.io setuppp

--- server/src/app.js
+++ server/src/app.js
@@ -28,6 +28,7 @@ import notificationRoutes from "./routes/notification.routes.js";
 import leaderboardRoutes from "./routes/leaderboard.routes.js";
 import announcementRoutes from "./routes/announcement.routes.js";
 import pyqsRoutes from "./routes/pyqs.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 
 //routes use
 app.use("/api/auth", authroutes)
@@ -41,5 +42,6 @@ app.use("/api/notifications", notificationRoutes)
 app.use("/api/leaderboard", leaderboardRoutes)
 app.use("/api/announcements", announcementRoutes)
 app.use("/api/pyqs", pyqsRoutes)
+app.use("/api/messages", messageRoutes)     
 
 export default app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,25 @@
-import app from './app.js';
-import {connectDb} from "./db/db.connection.js"
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import app from './app.js'
+import { connectDb } from "./db/db.connection.js"
+import { initChat } from './socket/chat.socket.js'
 
 connectDb()
-.then( () => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
+.then(() => {
+  const server = createServer(app)
+  const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 })
-.catch( (error) => {
-  console.error('Failed to connect to the database:', error);
+
+  initChat(io)
+
+  server.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+  })
+})
+.catch((error) => {
+  console.error('Failed to connect to the database:', error)
 })
