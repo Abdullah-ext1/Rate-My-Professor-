ea37c3a5081cdb5e94bc76ed84e82fc9c8c8a817
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-04-04 00:11:43 +0530

    online user update

--- server/src/socket/chat.socket.js
+++ server/src/socket/chat.socket.js
@@ -1,29 +1,36 @@
 import { Message } from "../models/message.models.js"
 import { socketAuth } from "../middlewares/socketAuth.js"
 
+let onlineUsers = []
+
 export function initChat(io) {
   io.use(socketAuth)
   io.on("connection", (socket) => {
     console.log("user connected:", socket.id)
 
+    onlineUsers.push({
+      name: socket.user.name,
+      college: socket.user.college,
+      socketId: socket.id
+    })
+    io.emit("onlineUsers", onlineUsers)
+
     socket.on("sendMessage", async (data) => {
       try {
-        //create message in MongoDB
         const message = await Message.create({
           sender: socket.user._id,
           college: socket.user.college,
           content: data.content,
         })
-
-        //broadcast to everyone with io.emit
-        io.emit("message", message);
-
+        io.emit("message", message)
       } catch (error) {
         socket.emit("error", { message: "Failed to send message" })
       }
     })
 
     socket.on("disconnect", () => {
+      onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
+      io.emit("onlineUsers", onlineUsers)
       console.log("user disconnected:", socket.id)
     })
   })
