b7e2f2f2d55e8c10a67476a172794b6ea0caa75c
Abdullah-ext1 <khanabdullah11915@gmail.com>
2026-04-17 11:35:44 +0530

    donee fixx

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bub75d67du"
+    "revision": "0.agkh6iit7n"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
